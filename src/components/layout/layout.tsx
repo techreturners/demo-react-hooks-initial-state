461f2494bbb630d170fb6d4c3c182acba95bfc75
neil-hughes <neil.hughes@techreturners.com>
2023-01-12 14:53:20 +0000

    fix: Use explicit return in `layout.tsx`

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,11 +2,14 @@ import { Footer } from "../footer/footer";
 import { Header } from "../header/header";
 import { DisplayJoke } from "../joke/joke";
 
-export const Layout: React.FC = () => <div className="App flex flex-col" style={{ 'height': '100vh' }}>
-	<Header />
-	<main className='pt-6 px-4 bg-neutral-100 flex-grow'>
-		<DisplayJoke />
-	</main>
-	<Footer />
-</div>;
+export const Layout: React.FC = () => {
+
+	return (<div className="App flex flex-col" style={{ 'height': '100vh' }}>
+		<Header />
+		<main className='pt-6 px-4 bg-neutral-100 flex-grow'>
+			<DisplayJoke />
+		</main>
+		<Footer />
+	</div>);
+}
 
